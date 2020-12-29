0e47d79a8bcb487d6203917ded9df9a51f8f8e06
Arjun <arjunbhd1194@gmail.com>
2020-12-29 18:20:07 +0530

    Added Sounds and notification

--- FirebaseNotifications/app/src/main/java/com/arjun1194/firebasenotifications/MainActivity.kt
+++ FirebaseNotifications/app/src/main/java/com/arjun1194/firebasenotifications/MainActivity.kt
@@ -1,18 +1,33 @@
 package com.arjun1194.firebasenotifications
 
+import android.media.AudioAttributes
+import android.media.AudioManager
+import android.media.MediaPlayer
+import android.media.RingtoneManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.arjun1194.firebasenotifications.notifications.NotificationWrapper
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val button: Button = findViewById<Button>(R.id.btn)
 
+        // remove this later
+        val notificationWrapper = NotificationWrapper(this).run {
+            removeCurrentNotification()
+            //NotificationWrapper.mediaPlayer.release();
+        }
+
+        // stop playing sound
         button.setOnClickListener {
-            println("Hi")
+            //mediaPlayer.start()
         }
     }
+
+
 }
--- FirebaseNotifications/app/src/main/java/com/arjun1194/firebasenotifications/MyApplication.kt
+++ FirebaseNotifications/app/src/main/java/com/arjun1194/firebasenotifications/MyApplication.kt
@@ -5,6 +5,10 @@ import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.media.AudioAttributes
+import android.media.AudioManager
+import android.media.RingtoneManager
+import android.net.Uri
 import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.ktx.initialize
@@ -23,7 +27,16 @@ class MyApplication: Application() {
     }
 
     private fun createNotificationChannel(){
+        val defaultRingtoneUri: Uri = RingtoneManager.getActualDefaultRingtoneUri(
+            this,
+            RingtoneManager.TYPE_RINGTONE
+        )
         val channel = NotificationChannel(channelId, channelDesc, NotificationManager.IMPORTANCE_HIGH)
+        val audioAttributes = AudioAttributes.Builder()
+            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+            .setUsage(AudioAttributes.USAGE_VOICE_COMMUNICATION)
+            .build()
+        channel.setSound(defaultRingtoneUri,audioAttributes)
         val notificationManager = getSystemService(NotificationManager::class.java)
         notificationManager.createNotificationChannel(channel)
     }
